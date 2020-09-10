4a38770eed2f70afd9cfe8ddc1b80cb72279b342
devdhar04 <dev.soni04@gmail.com>
2020-09-11 00:13:08 +0900

    Code configured

---
+++ src/main/kotlin/executeCollectPrChanges.kts
@@ -0,0 +1,19 @@
+import kotlin.system.exitProcess
+
+//DEPS com.squareup.moshi:moshi:1.9.3
+//DEPS com.squareup.moshi:moshi-kotlin:1.9.3
+//DEPS com.squareup.retrofit2:retrofit:2.9.0
+//DEPS com.squareup.retrofit2:converter-moshi:2.9.0
+
+//INCLUDE logging.kt
+//INCLUDE common.kt
+//INCLUDE collectPrChanges.kt
+//INCLUDE createGithubEvent.kt
+
+val result = "Hello New Action"
+
+if (result != 0) {
+    exitProcess(result)
+}
+
+println("Changes collected")
